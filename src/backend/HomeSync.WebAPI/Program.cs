a23d4f003aa8cf619997b6cbe42574e6dd6ff6ce
a-mahir-d <amd.coding.ac@gmail.com>
2026-06-11 11:26:09 +0300

    open source env variable RabbitMQ.MassTransit

--- src/backend/HomeSync.WebAPI/Program.cs
+++ src/backend/HomeSync.WebAPI/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddOptions<DemoUserSettings>().BindConfiguration("DemoUser").Va
 builder.Services.AddOptions<JwtSettings>().BindConfiguration("Jwt").ValidateDataAnnotations().ValidateOnStart();
 builder.Services.AddOptions<RabbitMQSettings>().BindConfiguration("RabbitMQ").ValidateDataAnnotations().ValidateOnStart();
 
+Environment.SetEnvironmentVariable("MT_LICENSE", "open-source");
 builder.Services.AddMassTransit(x =>
 {
     var rabbitMQSettings = builder.Configuration.GetSection("RabbitMQ").Get<RabbitMQSettings>() ?? throw new InvalidOperationException("RabbitMQSettings configuration is missing.");
